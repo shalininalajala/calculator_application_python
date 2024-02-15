4cedfb90aba17b0dcacd88658902ad4ed73b2d07
Ravindra Reddy <148531100+Ravindra0849@users.noreply.github.com>
2024-02-15 17:42:44 +0530

    SR

--- start_container.sh
+++ start_container.sh
@@ -4,8 +4,8 @@ set -e
 # Pull the Docker image from Docker Hub
 # echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin "$DOCKER_REGISTRY_URL"
 # docker pull "$DOCKER_REGISTRY_USERNAME/calculator-application:${CODEBUILD_BUILD_NUMBER}"
-docker pull ravisree900/calculator-application
+docker pull ravisree900/calculator-application:2
 
 # Run the Docker image as a container
 # docker run -d -p 5000:5000 "$DOCKER_REGISTRY_USERNAME/calculator-application:${CODEBUILD_BUILD_NUMBER}"
-docker run -d -p 5000:5000 ravisree900/calculator-application
+docker run -d -p 5000:5000 ravisree900/calculator-application:2
