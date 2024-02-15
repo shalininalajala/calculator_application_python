75fa193b010344ca1396586f123b52fbe8870ada
Ravindra Reddy <148531100+Ravindra0849@users.noreply.github.com>
2024-02-15 17:34:58 +0530

    SR

--- stop_container.sh
+++ stop_container.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-# To print the Container Id of running containers
-echo "Hello"
-# container_id = 'docker ps | awk -F " " '{print $1}''
-
 # Stop the running container (if any)
-# docker rm -f container_id
+echo "Hello"
