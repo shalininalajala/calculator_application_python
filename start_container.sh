2053857f24e5a0e4512f0b7377208edc3f21ed9f
Ravindra Reddy <148531100+Ravindra0849@users.noreply.github.com>
2024-02-15 16:51:38 +0530

    new commit

--- start_container.sh
+++ start_container.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
+echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin "$DOCKER_REGISTRY_URL"
 docker pull "$DOCKER_REGISTRY_USERNAME/calculator-application:${CODEBUILD_BUILD_NUMBER}"
 
 # Run the Docker image as a container
